449f5194e955918b1d16e2935a5fe664a1df785e
Sowndarya236 <120468745+Sowndarya236@users.noreply.github.com>
2025-07-09 12:28:59 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import UserTable from "../components/UserTable";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <UserTable />
+    </>
+  );
+};
+
+export default Dashboard;
